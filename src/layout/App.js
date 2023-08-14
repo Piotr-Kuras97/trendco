26fe23544cfe22afaa7ddbe3cbb161830fd0e618
Piotr Kuraś <piotr.kuras.1997@gmail.com>
2023-08-14 18:10:39 +0200

    initial commit

---
+++ src/layout/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '../sass/main.scss'
+
+function App() {
+  return (
+    <Router>
+      <p>App</p>
+
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
